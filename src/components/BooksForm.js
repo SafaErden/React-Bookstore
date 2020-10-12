4cd9182fefb24c689f3a7a0107bec64f03342868
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-10-12 16:42:20 +0300

    make bookform component a stateful component

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,18 +1,35 @@
 import React from 'react';
 
-const category = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
-const Books = () => (
-  <div className="bookform-section">
-    <h4>ADD NEW BOOK</h4>
-    <input type="text" />
-    <select>
-      {category.forEach(item => (
-        <option>{item}</option>
-      ))}
-    </select>
-    <button type="submit">Add a new book</button>
-  </div>
-);
+export default class BooksForm extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      title:'',
+      category: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e){
+    this.setState({[e.target.name]: e.target.value});
+  }
+  render() {
+    const {title, category} = this.state;
+    return (
+      <div className="bookform-section">
+        <form>
+          <h4>ADD NEW BOOK</h4>
+          <input type="text" onChange={this.handleChange} value={title} name="title" placeHolder="Type Book Title"/>
+          <select onChange={this.handleChange} name="category">
+            {categories.map((item) => (
+              <option value={item}>{item}</option>
+            ))}
+          </select>
+          <button type="submit">Add a new book</button>
+        </form>
+      </div>
 
-export default Books;
+    );
+  }
+}
