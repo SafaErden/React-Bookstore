e55cb1c99bd0a844b65d55d2d529786e1df757ef
taiwocoker <taiwofcoker@gmail.com>
2020-10-13 15:32:14 +0100

    Change background color

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import BooksForm from '../containers/BooksForm';
 
 function App() {
   return (
-    <div className="bg-light min-vh-100 d-flex align-items-center justify-content-center flex-column p-5">
-      <BooksList key={12345} />
+    <div className="bg-light min-vh-100 d-flex align-items-center justify-content-center flex-column p-5 container">
+      <BooksList />
       <BooksForm />
     </div>
   );
